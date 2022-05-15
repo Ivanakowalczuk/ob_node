165dc9c6d15a8e3d1c5b11ef3f306df2f56d2432
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2022-05-15 13:15:25 -0300

    second commit

---
+++ listas.js
@@ -0,0 +1,6 @@
+
+let array=["Ivana", 39, true, new Date(1982,9,25,0,0,0) ]
+let libroFavorito={tittulo:"Harry Potter", autor:"Rowling", fecha: new Date(1997,5,6,0,0,0),  url: "https://books.google.com.ar/books/about/Harry_Potter_y_la_piedra_filosofal.html?id=2zgRDXFWkm8C&printsec=frontcover&source=kp_read_button&hl=es-419&redir_esc=y#v=onepage&q&f=false" }
+
+array.push(libroFavorito)
+console.log(array)
